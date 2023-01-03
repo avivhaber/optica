a730acabe8f7eedce457441a7bc39bc147db7ace
Aviv Haber <avivh72@gmail.com>
2023-01-03 10:29:50 -0500

    Format

--- src/core/Renderer.h
+++ src/core/Renderer.h
@@ -38,8 +38,8 @@ class Renderer {
   float reflectance{0.5f};
 
   Renderer(int width, int height, int samplesPerPixel = 100, int maxDepth = 20);
-  Renderer(int width, int height, float fov, float focalLength, int samplesPerPixel = 100,
-           int maxDepth = 20);
+  Renderer(int width, int height, float fov, float focalLength,
+           int samplesPerPixel = 100, int maxDepth = 20);
   Renderer(const Camera& camera, int samplesPerPixel = 100, int maxDepth = 20);
 
   // Renders the scene and saves the rendered image to disk.
